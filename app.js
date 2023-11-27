37be4f2aa8db84426cb044cf2b68ad5e5015df1d
holvicks <olamilekansegun71@gmail.com>
2023-11-27 12:04:10 +0100

    hackathon challeng commit

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ let alertBtn = document.getElementById('alertBtn');
 let alertBox = document.getElementById('alertBox');
 let userProfile = document.getElementById('userProfile');
 let userProfileAlert = document.getElementById('userProfileAlert')
-
+let heroid = document.getElementById('heroid');
+let cancelhero = document.getElementById('cancelhero');
+let cancelhero2 = document.getElementById('cancelhero2')
 
 function closeAlertBox() {
     alertBox.classList.remove('show');
@@ -27,10 +29,27 @@ window.addEventListener('click', function(){
     closeAlertBox();
 })
 
+cancelhero.addEventListener('click', function() {
+  heroid.style.display = 'none';
+})
+
+cancelhero.addEventListener('click', function() {
+  heroid.style.display = 'none';
+})
 
 
 
-let currentIconIndex = 0;
+let slider = document.getElementById("range");
+let output = document.getElementById("demo");
+let iconContainerCount = 0; // Variable to keep track of the count of icon-container elements clicked
+
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
+
+let currentIconIndexes = {};
 const icons = [
   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><circle cx="12" cy="12" r="10" stroke="#8A8A8A" stroke-width="2.08333" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="5 5"/></svg>',
   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><circle cx="12" cy="12" r="10" stroke="#8A8A8A" stroke-width="2.08333" stroke-linecap="round" stroke-linejoin="round"/></svg>',
@@ -43,44 +62,145 @@ const icons = [
 const icons2 = [
   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><circle cx="12" cy="12" r="10" stroke="#8A8A8A" stroke-width="2.08333" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="5 5"/></svg>',
 ];
-let animationInterval;
 
-function toggleIcon() {
-  const iconContainer = document.querySelector('.icon-container');
-  if (currentIconIndex < icons.length) {
-    iconContainer.innerHTML = icons[currentIconIndex];
-    console.log('hello');
+let animationIntervals = {};
+
+function toggleIcon(container) {
+  const heroId = container.getAttribute("data-hero-id");
+
+  if (!currentIconIndexes[heroId]) {
+    currentIconIndexes[heroId] = 0;
+  }
+
+  if (currentIconIndexes[heroId] < icons.length) {
+    container.innerHTML = icons[currentIconIndexes[heroId]];
   } else {
-    const icons2Index = currentIconIndex - icons.length;
-    iconContainer.innerHTML = icons2[icons2Index];
-    console.log('hello2');
+    const icons2Index = currentIconIndexes[heroId] - icons.length;
+    container.innerHTML = icons2[icons2Index];
   }
-  currentIconIndex = (currentIconIndex + 1) % (icons.length + icons2.length);
 
-  if (currentIconIndex === icons.length + icons2.length - 1) {
-    // If the last icon is reached, reset to the first icon
-    clearInterval(animationInterval);
-    iconContainer.addEventListener('click', resetToDefault, { once: true });
+  currentIconIndexes[heroId] =
+    (currentIconIndexes[heroId] + 1) % (icons.length + icons2.length);
+
+  if (currentIconIndexes[heroId] === icons.length + icons2.length - 1) {
+    clearInterval(animationIntervals[heroId]);
+    container.addEventListener("click", () => resetToDefault(heroId), {
+      once: true,
+    });
+
+    // Update range slider value and count when icon-container is clicked
+    iconContainerCount++;
+    container.classList.add("clicked"); // Add the 'clicked' class
+    updateSlider();
   }
+
+  // Stop the click event propagation
+  container.addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
 }
 
-function resetToDefault() {
-  currentIconIndex = 0;
-  const iconContainer = document.querySelector('.icon-container');
-  iconContainer.innerHTML = icons[currentIconIndex];
-  iconContainer.removeEventListener('click', resetToDefault);
+function resetToDefault(heroId) {
+  currentIconIndexes[heroId] = 0;
+  const container = document.querySelector(
+    `.icon-container[data-hero-id="${heroId}"]`
+  );
+  container.innerHTML = icons[currentIconIndexes[heroId]];
+  container.removeEventListener("click", () => resetToDefault(heroId));
 
   // Clear the animation interval
-  clearInterval(animationInterval);
+  clearInterval(animationIntervals[heroId]);
 
   // Call toggleIcon once to display the default icon immediately
-  toggleIcon();
+  toggleIcon(container);
+
+  // Update range slider value and count when icon-container is reset to default
+  iconContainerCount--;
+  container.classList.remove("clicked"); // Remove the 'clicked' class
+  updateSlider();
 }
 
-document.querySelector('.icon-container').addEventListener('click', function () {
-  clearInterval(animationInterval);
-  animationInterval = setInterval(toggleIcon, 50);
+function updateSlider() {
+  const clickedIconsCount = document.querySelectorAll(".icon-container.clicked")
+    .length;
+  const notClickedIconsCount = iconContainerCount - clickedIconsCount;
+
+  // Update range slider value and count based on clicked and not clicked icons
+  slider.value = clickedIconsCount;
+  output.innerHTML = `${slider.value}/5`;
+
+  // Optionally, you can also update based on not clicked icons
+  // slider.value = notClickedIconsCount;
+  // output.innerHTML = `${slider.value}/5`;
+}
+
+document.querySelectorAll(".icon-container").forEach((container) => {
+  const heroId = container.getAttribute("data-hero-id");
+  container.addEventListener("click", () => {
+    clearInterval(animationIntervals[heroId]);
+    animationIntervals[heroId] = setInterval(() => toggleIcon(container), 50);
+  });
+
+  // Initial call to display the default icon for each hero content
+  resetToDefault(heroId);
 });
 
-// Initial call to display the default icon
-resetToDefault();
+
+
+
+
+
+
+const items = document.querySelectorAll(".accordion button");
+const arrowDown = document.querySelector(".arrowDown");
+const subHeader = document.querySelector(".sub__header");
+const accordionContainer = document.querySelector(".accordion");
+const range = document.querySelector("#range");
+const accordionContent = document.querySelectorAll(".accordion-item");
+
+range.value = "2";
+let isOpen = false;
+
+function toggleAccordionContainer() {
+  isOpen = !isOpen;
+  const arrowIcon = `
+<img
+  src="${
+    isOpen
+      ? "https://crushingit.tech/hackathon-assets/icon-arrow-up.svg"
+      : "https://crushingit.tech/hackathon-assets/icon-arrow-down.svg"
+  }"
+  alt="arrowDown"
+/>`;
+
+  arrowDown.innerHTML = arrowIcon;
+
+  // Toggle the display property of the accordion container
+  accordionContainer.style.display = isOpen ? "block" : "none";
+}
+
+function toggleAccordion(e) {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  // If the clicked item is already expanded, collapse it
+  if (itemToggle === "true") {
+    this.setAttribute("aria-expanded", "false");
+    this.parentElement.classList.remove("active");
+  } else {
+    // If the clicked item is not expanded, expand it and collapse others
+    items.forEach((item) => {
+      item.setAttribute("aria-expanded", "false");
+      item.parentElement.classList.remove("active");
+    });
+
+    this.setAttribute("aria-expanded", "true");
+    this.parentElement.classList.add("active");
+  }
+}
+// Initially hide the accordion container
+accordionContainer.style.display = "none";
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
+subHeader.addEventListener("click", toggleAccordionContainer);
+
+
